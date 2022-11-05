f0910cc4ec5aedcbcec33ac382dc80ee289a01ff
Jeon Minji <mjjeon2645@gmail.com>
2022-11-05 14:32:30 +0900

    필터 로직 일부 수정(예외처리 추가)

---
+++ src/main/java/kr/megaptera/wherewego/exceptions/CategoryFilterNotSelected.java
@@ -0,0 +1,7 @@
+package kr.megaptera.wherewego.exceptions;
+
+public class CategoryFilterNotSelected extends RuntimeException {
+  public CategoryFilterNotSelected() {
+    super("가고싶은 장소의 유형을 선택해주세요!");
+  }
+}
