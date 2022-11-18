7d15a84e8a277957d9ff77f4057e337fc3a5fc67
Jeon Minji <mjjeon2645@gmail.com>
2022-11-18 23:13:33 +0900

    임시저장(자리이동)

---
+++ src/main/java/kr/megaptera/wherewego/infrastructure/KakaoLoginService.java
@@ -0,0 +1,18 @@
+package kr.megaptera.wherewego.infrastructure;
+
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+import org.springframework.web.reactive.function.client.*;
+
+import javax.annotation.*;
+
+@Service
+@Transactional
+public class KakaoLoginService {
+    private WebClient webClient;
+
+    @PostConstruct
+    public void initWebClient() {
+        webClient = WebClient.create("")
+    }
+}
