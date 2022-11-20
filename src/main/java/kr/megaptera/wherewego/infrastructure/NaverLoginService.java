4a2e505268b67aa201ff04e8e4a9d3885bc9a924
Jeon Minji <mjjeon2645@gmail.com>
2022-11-20 09:37:35 +0900

    네이버 로그인 관련 액세스 토큰 및 사용자 정보 불러오는 로직 1차 구현

--- src/main/java/kr/megaptera/wherewego/infrastructure/NaverLoginService.java
+++ src/main/java/kr/megaptera/wherewego/infrastructure/NaverLoginService.java
@@ -13,24 +13,30 @@ import java.util.*;
 
 @Service
 @Transactional
-public class KakaoLoginService {
-    @Value("${kakao.api-key}")
+public class NaverLoginService {
+    @Value("${naver.api-key}")
     private String apiKey;
 
-    @Value("${kakao.redirect-uri}")
+    @Value("${naver.redirect-uri}")
     private String redirectUri;
 
-    private Map<String, String> kakaoUserInformation;
+    @Value("${naver.secret}")
+    private String secret;
 
-    public KakaoLoginService() {
-        kakaoUserInformation = new LinkedHashMap<>();
+    @Value("${naver.state-string}")
+    private String state;
+
+    private Map<String, String> naverUserInformation;
+
+    public NaverLoginService() {
+        naverUserInformation = new LinkedHashMap<>();
     }
 
-    public LoginResultDto kakaoLogin(String code) {
+    public LoginResultDto naverLogin(String code) {
         String accessToken = getAccessToken(code);
         getDetail(accessToken);
 
-        return new LoginResultDto(accessToken, kakaoUserInformation.get("nickName"));
+        return new LoginResultDto(accessToken, naverUserInformation.get("nickName"));
     }
 
     public String getAccessToken(String code) {
@@ -44,27 +50,31 @@ public class KakaoLoginService {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", apiKey);
-        params.add("redirect_uri", redirectUri);
+        params.add("client_secret", secret);
         params.add("code", code);
+        params.add("state", state);
+        params.add("redirect_uri", redirectUri);
 
         // HttpHeader와 HttpBody를 하나의 객체에 담기 -> 만든 이유 : 아래의 exchange 함수에 HttpEntity를 넣어야 해서..
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+        HttpEntity<MultiValueMap<String, String>> naverTokenRequest =
             new HttpEntity<>(params, httpHeaders); // body 데이터와 headers 값을 가지고 있는 Entity
 
-        // 카카오에게 Http 요청하기 (POST 방식) -> response라는 변수에 응답을 받음
+        // 네이버에게 Http 요청하기 (POST 방식) -> response라는 변수에 응답을 받음
         String response = restTemplate.exchange(
-            "https://kauth.kakao.com/oauth/token",
+            "https://nid.naver.com/oauth2.0/token",
             HttpMethod.POST,
-            kakaoTokenRequest,
+            naverTokenRequest,
             String.class).getBody();
 
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(response);
         String accessToken = element.getAsJsonObject().get("access_token").getAsString();
         String refreshToken = element.getAsJsonObject().get("refresh_token").getAsString();
+        String expiresIn = element.getAsJsonObject().get("expires_in").getAsString();
 
-        kakaoUserInformation.put("accessToken", accessToken);
-        kakaoUserInformation.put("refreshToken", refreshToken);
+        naverUserInformation.put("accessToken", accessToken);
+        naverUserInformation.put("refreshToken", refreshToken);
+        naverUserInformation.put("expires_in", expiresIn);
 
         return accessToken;
     }
@@ -79,27 +89,25 @@ public class KakaoLoginService {
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         // HttpHeader와 HttpBody 담기
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers);
+        HttpEntity<MultiValueMap<String, String>> naverProfileRequest = new HttpEntity<>(headers);
 
         String response = restTemplate.exchange(
-            "https://kapi.kakao.com/v2/user/me",
+            "https://openapi.naver.com/v1/nid/me",
             HttpMethod.GET,
-            kakaoProfileRequest,
+            naverProfileRequest,
             String.class).getBody();
 
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(response);
 
-        JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
-        JsonObject kakaoAccount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+        JsonObject naverAccount = element.getAsJsonObject().get("response").getAsJsonObject();
 
-        String id = element.getAsJsonObject().get("id").getAsString();
-        String nickname = properties.getAsJsonObject().get("nickname").getAsString();
-        String email = kakaoAccount.getAsJsonObject().get("email").getAsString();
+        String id = naverAccount.getAsJsonObject().get("id").getAsString();
+        String nickname = naverAccount.getAsJsonObject().get("nickname").getAsString();
+        String email = naverAccount.getAsJsonObject().get("email").getAsString();
 
-        kakaoUserInformation.put("kakaoUserId", id);
-        kakaoUserInformation.put("nickName", nickname);
-        kakaoUserInformation.put("email", email);
-
+        naverUserInformation.put("naverUserId", id);
+        naverUserInformation.put("nickName", nickname);
+        naverUserInformation.put("email", email);
     }
 }
