353617f4b6f8418490f150d728cf668e15da0496
Jeon Minji <mjjeon2645@gmail.com>
2022-11-30 19:24:21 +0900

    사용자들이 작성한 리뷰를 모두 프론트로 보내주는 로직 추가

--- src/test/java/kr/megaptera/wherewego/admins/AdminPlaceControllerTest.java
+++ src/test/java/kr/megaptera/wherewego/admins/AdminPlaceControllerTest.java
@@ -1,5 +1,6 @@
 package kr.megaptera.wherewego.admins;
 
+import kr.megaptera.wherewego.models.*;
 import kr.megaptera.wherewego.services.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.*;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdminPlaceController.class)
@@ -18,9 +21,20 @@ class AdminPlaceControllerTest {
     @MockBean
     private GetPlaceService getPlaceService;
 
+    @BeforeEach
+    void setUp() {
+        given(getPlaceService.selectedPlace(5L)).willReturn(Place.fake1(5L, "자연"));
+    }
+
     @Test
     void places() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/admin-places"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void selectedPlace() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin-places/5"))
             .andExpect(status().isOk());
     }
 }
