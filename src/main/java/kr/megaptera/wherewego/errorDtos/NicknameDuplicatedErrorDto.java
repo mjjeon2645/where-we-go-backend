f7054fa5763750e92ce4ace9e7b1eac9f2649e50
Jeon Minji <mjjeon2645@gmail.com>
2022-12-07 13:40:30 +0900

    사용자 회원가입 중 닉네임 중복 시 출력되는 에러메시지 일부 수정

--- src/main/java/kr/megaptera/wherewego/errorDtos/NicknameDuplicatedErrorDto.java
+++ src/main/java/kr/megaptera/wherewego/errorDtos/NicknameDuplicatedErrorDto.java
@@ -2,6 +2,6 @@ package kr.megaptera.wherewego.errorDtos;
 
 public class NicknameDuplicatedErrorDto extends ErrorDto{
     public NicknameDuplicatedErrorDto() {
-        super(1001, "이미 사용중인 닉네임입니다. 다른 닉네임을 입력해주세요");
+        super(1001, "이미 다른 분이 사용 중이예요");
     }
 }
--- src/main/java/kr/megaptera/wherewego/exceptions/NicknameDuplicatedException.java
+++ src/main/java/kr/megaptera/wherewego/exceptions/NicknameDuplicatedException.java
@@ -2,6 +2,6 @@ package kr.megaptera.wherewego.exceptions;
 
 public class NicknameDuplicatedException extends RuntimeException {
     public NicknameDuplicatedException() {
-        super("이미 사용중인 닉네임입니다. 다른 닉네임을 입력해주세요");
+        super("이미 다른 분이 사용중이예요");
     }
 }
